d242ee5c3e63c232db440e638a498fccb974447b
Aki Wu <wuminzhe@gmail.com>
2023-04-06 05:56:35 +0800

    add pangolin2 config

--- config/config.rb
+++ config/config.rb
@@ -1,8 +1,10 @@
 def config 
   {
     url: 'https://crab-rpc.darwinia.network',
+    pangolin_url: 'https://pangolin-rpc.darwinia.network',
     metadata: {
-      crab2: File.join(__dir__, './metadata/crab2.json')
+      crab2: File.join(__dir__, './metadata/crab2.json'),
+      pangolin2: File.join(__dir__, './metadata/pangolin2.json')
     }
   }
 end
