46fc6d348fec5591c97057851335256933cb1627
Aki Wu <wuminzhe@gmail.com>
2023-04-06 10:25:09 +0800

    remove gas_limit from /pangolin/encode_transact_call

--- server.rb
+++ server.rb
@@ -51,13 +51,12 @@ end
 post '/pangolin/encode_transact_call' do
   ethereum_contract = params[:ethereum_contract][2..];
   ethereum_call = params[:ethereum_call][2..];
-  gas_limit = PortableCodec.u256(params[:gas_limit].to_i)
 
   pangolin_endpoint = "0x5a07DB2bD2624DD2Bdd5093517048a0033A615b5"
  
   # get market fee from pangolin endpoint
   fee = ScaleRb::HttpClient.json_rpc_call(config[:pangolin_url], 'eth_call', {data: "0xddca3f43",gas: "0x5b8d80",to: pangolin_endpoint}, "latest")
-  fee = PortableCodec.u256(fee.to_i(16)) 
+  fee = PortableCodec.u256(fee.to_i(16) * 1.5) 
 
   # calculate the call data of `executeOnEthereum` function of pangolin endpoint
   call_length_hex = (ethereum_call.length / 2).to_s(16);
@@ -71,7 +70,7 @@ post '/pangolin/encode_transact_call' do
           :transact=>{
             :xcm_transaction=>{
               :V2=>{
-                :gas_limit=>gas_limit, 
+                :gas_limit=>PortableCodec.u256(600000), 
                 :action=>{
                   :Call=>pangolin_endpoint.to_bytes
                 }, 
