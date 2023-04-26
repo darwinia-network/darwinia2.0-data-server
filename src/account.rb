5a1bc451e4f362af021e10b6d0b3c972a8b086b0
Aki Wu <wuminzhe@gmail.com>
2023-04-26 10:52:10 +0800

    fix account calculation

--- src/account.rb
+++ src/account.rb
@@ -11,16 +11,17 @@ def get_account_info(rpc, metadata, address)
   reserved = info[:data][:reserved].to_f / 10**18
 
   # KTON
-  kton_free =
+  kton_info =
     ScaleRb::HttpClient.get_storage2(
       rpc,
       "Assets",
       "Account",
       [1026] + param,
       metadata,
-    )[
+    )
+  kton_free = kton_info ? kton_info[
       :balance
-    ].to_f / 10**18
+    ].to_f / 10**18 : 0
 
   # LOCKS
   locks =
@@ -36,13 +37,13 @@ def get_account_info(rpc, metadata, address)
       param,
       metadata,
     )
-  deposits =
+  deposits = deposits ? 
     deposits.map do |deposit|
       deposit[:value] = deposit[:value].to_f / 10**18
       deposit[:start_time] = Time.at(deposit[:start_time] / 1000)
       deposit[:expired_time] = Time.at(deposit[:expired_time] / 1000)
       deposit
-    end
+    end : []
 
   # STAKING LEDGER
   ledger =
@@ -53,30 +54,32 @@ def get_account_info(rpc, metadata, address)
       param,
       metadata,
     )
-  ledger[:staked_ring] = ledger[:staked_ring].to_f / 10**18
-  ledger[:unstaking_ring] = ledger[:unstaking_ring].map do |item|
-    [item[0].to_f / 10**18, item[1]]
-  end
-  ledger[:staked_kton] = ledger[:staked_kton].to_f / 10**18
-  ledger[:unstaking_kton] = ledger[:unstaking_kton].map do |item|
-    [item[0].to_f / 10**18, item[1]]
-  end
+
+  staked_ring =  ledger ? ledger[:staked_ring].to_f / 10**18 : 0
+  unstaking_ring = ledger ? 
+    (ledger[:unstaking_ring].map do |item|
+      [item[0].to_f / 10**18, item[1]]
+    end.reduce(0) { |sum, item| sum + item[0] } )
+    : 0
+  staked_kton = ledger ? ledger[:staked_kton].to_f / 10**18 : 0
+  unstaking_kton = ledger ? 
+    (ledger[:unstaking_kton].map do |item|
+      [item[0].to_f / 10**18, item[1]]
+    end.reduce(0) { |sum, item| sum + item[0] } )
+    : 0
 
   {
     ring:
-      free + reserved + ledger[:staked_ring] +
-        ledger[:unstaking_ring].reduce(0) { |sum, item| sum + item[0] } +
-        deposits.reduce(0) { |sum, deposit| sum + deposit[:value] },
+      free + reserved + staked_ring + unstaking_ring + deposits.reduce(0) { |sum, deposit| sum + deposit[:value] },
     kton:
-      kton_free + ledger[:staked_kton] +
-        ledger[:unstaking_kton].reduce(0) { |sum, item| sum + item[0] },
+      kton_free + staked_kton + unstaking_kton,
     items: {
       transferable: free - locked,
       reserved: reserved,
       locked: locked,
       deposits: deposits,
-      staking: ledger,
-    },
+      staking: ledger
+    }
   }
 end
 
